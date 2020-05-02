8bf37d2dc534c1e12def979ecf5d7dc948b2ae85
ErwinWoz <eerwoz@gmail.com>
2020-05-02 11:36:45 -0700

    added Week 4 Assignmen

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,12 @@ $(function () {
             $("#carouselButton").children("span").addClass("fa-pause");
         }
     });
+
+    $("#reserveButton").click(function () {
+        $("#reserveModal").modal("show");
+    });
+
+    $("#loginButton").click(function () {
+        $("#loginModal").modal("show");
+    });
 });
